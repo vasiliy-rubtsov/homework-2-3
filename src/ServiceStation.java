e1f4c6254410bd41bc27266956326358a9fb14bf
Vasiliy Rubtsov <vasiliy.rubtsov@rtmis.ru>
2024-10-28 15:39:20 +0300

    Домашнее задание 2-3

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,23 +1,48 @@
 public class ServiceStation {
-    public void check(Car car, Bicycle bicycle, Truck truck) {
-        if (car != null) {
-            System.out.println("Обслуживаем " + car.modelName);
-            for (int i = 0; i < car.wheelsCount; i++) {
-                car.updateTyre();
-            }
-            car.checkEngine();
-        } else if (truck != null) {
-            System.out.println("Обслуживаем " + truck.modelName);
-            for (int i = 0; i < truck.wheelsCount; i++) {
-                truck.updateTyre();
-            }
-            truck.checkEngine();
-            truck.checkTrailer();
-        } else if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.modelName);
-            for (int i = 0; i < bicycle.wheelsCount; i++) {
-                bicycle.updateTyre();
-            }
+
+    private void updateTyres(TransportVehicle vehicle) {
+        for (int i = 0; i < vehicle.getWheelsCount(); i++) {
+            vehicle.updateTyre();
+        }
+    }
+
+    /**
+     * Техобслуживание легковых автомобилей
+     * @param vehicle Car
+     */
+    public void check(Car vehicle) {
+        System.out.println("Обслуживаем " + vehicle.getModelName());
+        updateTyres(vehicle);
+        vehicle.checkEngine();
+    }
+
+    /**
+     * Техобслуживание велосипедов
+     * @param vehicle Bicycle
+     */
+    public void check(Bicycle vehicle) {
+        System.out.println("Обслуживаем " + vehicle.getModelName());
+        updateTyres(vehicle);
+    }
+
+    /**
+     * Техобслуживание грузовиков
+     * @param vehicle Truck
+     */
+    public void check(Truck vehicle) {
+        System.out.println("Обслуживаем " + vehicle.getModelName());
+        updateTyres(vehicle);
+        vehicle.checkEngine();
+        vehicle.checkTrailer();
+    }
+
+    public void check(TransportVehicle vehicle) {
+        if (vehicle instanceof Car) {
+            check((Car) vehicle);
+        } else if (vehicle instanceof Truck) {
+            check((Truck) vehicle);
+        } else {
+            check((Bicycle) vehicle);
         }
     }
 }
